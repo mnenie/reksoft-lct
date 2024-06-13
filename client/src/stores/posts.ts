f484168e198a5661fe5265b0b388181943ec3ec2
sv022 <ssvatkin04@gmail.com>
2024-06-13 23:41:14 +0300

    add: backend API

--- client/src/stores/posts.ts
+++ client/src/stores/posts.ts
@@ -1,7 +1,9 @@
 import useFilteredNews from '@/composables/useFilteredNews';
-import type { IPost } from '@/types/post.interface';
+import type { IComment, IPost } from '@/types/post.interface';
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
+import PostService from '@/services/postsService';
+
 
 export const usePostStore = defineStore('posts', () => {
   const posts = ref<IPost[]>([]);
@@ -9,11 +11,78 @@ export const usePostStore = defineStore('posts', () => {
   const showCreateForm = ref<boolean>(false);
 
   const { filteredNotes } = useFilteredNews(posts, search);
+
+  const fetchPosts = async function() : Promise<IPost[]> {
+    try {
+      // const fetchedPosts : IPost[] = (await PostService.posts()).data || [];
+      const fetchedPosts = [] as IPost[];
+      return fetchedPosts;
+    } catch (err) {
+      console.log(err);
+      return [];
+    }
+  }
+  const postPost = async function(post: IPost) : Promise<void> {
+    try {
+      // const resp = await PostService.post(post);
+      // post = resp.data
+      posts.value.push(post);
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  const patchPost = async function(post: IPost) : Promise<void> {
+    try {
+      await PostService.patch(post);
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  const deletePost = async function(post: IPost) : Promise<void> {
+    try {
+      await PostService.delete(post);
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  const postComment = async function(post: IPost, comment: IComment) : Promise<IPost> {
+    try {
+      const resp = await PostService.postComment(post, comment);
+      return resp.data;
+    } catch (err) {
+      console.log(err)
+      return {} as IPost;
+    }
+  }
+  const postImage = async function(post: IPost) : Promise<void> {
+    try {
+      await PostService.post(post);
+      posts.value.push(post);
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  const postFile = async function(post: IPost) : Promise<void> {
+    try {
+      await PostService.post(post);
+      posts.value.push(post);
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   
   return {
     posts,
     search,
     showCreateForm,
-    filteredNotes
+    filteredNotes,
+    fetchPosts,
+    postPost,
+    patchPost,
+    deletePost,
+    postComment,
+    postFile,
+    postImage
   };
 });
