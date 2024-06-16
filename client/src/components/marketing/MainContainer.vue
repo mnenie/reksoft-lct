716e6f8b42ebd6a8d2d38b9a70a1ffe5fd162f27
sv022 <ssvatkin04@gmail.com>
2024-06-16 16:59:58 +0300

    add: post type filter

--- client/src/components/marketing/MainContainer.vue
+++ client/src/components/marketing/MainContainer.vue
@@ -1,6 +1,30 @@
 <script setup lang="ts">
-import { Button } from '@/components/ui/button';
+import { ref } from 'vue';
+import { usePostStore } from '@/stores/posts';
+import { storeToRefs } from 'pinia';
 import { redirect } from '@/helpers/helperRedirect';
+
+import { Button } from '@/components/ui/button';
+import { Label } from '@/components/ui/label'
+import { Switch } from '@/components/ui/switch'
+
+
+const postStore = usePostStore();
+const { typeSearch } = storeToRefs(postStore);
+
+
+const checkedArticle = ref<boolean>(true);
+const checkedRecruiter = ref<boolean>(true);
+const checkedApplicant = ref<boolean>(true);
+
+function handleCheck(type: string) {
+  if (typeSearch.value.includes(type)){
+    typeSearch.value = typeSearch.value.filter(t => t !== type)
+    return;
+  }
+  typeSearch.value.push(type);
+}
+
 </script>
 <template>
   <div class="flex flex-col gap-4">
@@ -17,6 +41,32 @@ import { redirect } from '@/helpers/helperRedirect';
         <img src="/github.png" class="w-4 h-4 mr-2" />
         GitHub
       </Button>
+    </div>
+    <div class="flex h-fit w-[300px] min-w-[300px] flex-col gap-2 rounded-md bg-white p-4">
+      <div class="flex items-center justify-between">
+        <Label for="article">Статьи</Label>
+        <Switch 
+          id="article" 
+          :checked="checkedArticle" 
+          @update:checked="handleCheck('article'); checkedArticle = !checkedArticle"
+          />
+      </div>
+      <div class="flex items-center justify-between">
+        <Label for="recruiter">Вакансии</Label>
+        <Switch 
+          id="recruiter" 
+          :checked="checkedRecruiter"
+          @update:checked="handleCheck('recruiter'); checkedRecruiter = !checkedRecruiter"
+          />
+      </div>
+      <div class="flex items-center justify-between">
+        <Label for="applicant">Резюме</Label>
+        <Switch 
+          id="applicant" 
+          :checked="checkedApplicant" 
+          @update:checked="handleCheck('applicant'); checkedApplicant = !checkedApplicant" 
+          />
+      </div>
     </div>
   </div>
 </template>
